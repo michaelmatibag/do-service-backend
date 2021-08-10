819c20482f75bcedc038fe3dd28d0a7d88a2143c
Michael Matibag <michaelmatibag@gmail.com>
2021-08-10 13:07:29 -0500

    Initial commit.

---
+++ Migrations/20210810174317_initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DOService.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "doi_headers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    organization_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    approved_flag = table.Column<bool>(type: "boolean", nullable: false),
+                    approved_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    approved_user_id = table.Column<string>(type: "text", nullable: true),
+                    created_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_doi_headers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "doi_owners",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    organization_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    doi_header_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    owner_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    owner_name = table.Column<string>(type: "text", nullable: true),
+                    pay_code = table.Column<string>(type: "text", nullable: true),
+                    suspense_reason = table.Column<string>(type: "text", nullable: true),
+                    interest_type = table.Column<string>(type: "text", nullable: true),
+                    nri_decimal = table.Column<decimal>(type: "numeric", nullable: false),
+                    burden_group_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    effective_from_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    effective_to_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    created_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_doi_owners", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "doi_headers");
+
+            migrationBuilder.DropTable(
+                name: "doi_owners");
+        }
+    }
+}
---
+++ Models/DoiOwner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DOService.Models
+{
+    [Table("doi_owners")]
+    public class DoiOwner
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("organization_id")]
+        public Guid OrganizationId { get; set; }
+
+        [Column("doi_header_id")]
+        public Guid DoiHeaderId { get; set; }
+
+        [Column("owner_id")]
+        public Guid OwnerId { get; set; }
+
+        [Column("owner_name")]
+        public string OwnerName { get; set; }
+
+        [Column("pay_code")]
+        public string PayCode { get; set; }
+
+        [Column("suspense_reason")]
+        public string SuspenseReason { get; set; }
+
+        [Column("interest_type")]
+        public string InterestType { get; set; }
+
+        [Column("nri_decimal")]
+        public decimal NriDecimal { get; set; }
+
+        [Column("burden_group_id")]
+        public Guid BurdenGroupId { get; set; }
+
+        [Column("effective_from_date")]
+        public DateTime EffectiveFromDate { get; set; }
+
+        [Column("effective_to_date")]
+        public DateTime EffectiveToDate { get; set; }
+
+        [Column("created_date")]
+        public DateTime CreatedDate { get; set; }
+    }
+}
